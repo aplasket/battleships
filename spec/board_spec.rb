07d4d7ad2817acc29277bf86e427db2c3d90d32d
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-07 16:29:26 -0600

    add validating coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe do
       expect(@board.cells.size).to eq(16)
     end
   end
+
+  describe '#validating coordinates' do
+    it 'will validate coordinates that are on the board' do
+      expect(@board.valid_coordinate?("A1")).to be(true)
+      expect(@board.valid_coordinate?("D4")).to be(true)
+      expect(@board.valid_coordinate?("A5")).to be(false)
+      expect(@board.valid_coordinate?("E1")).to be(false)
+      expect(@board.valid_coordinate?("A22")).to be(false)
+    end
+  end
+
+  describe '#validating placement'
 end
