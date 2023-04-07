be65d8f4789bda796f0f687f27c5b4a2ea743170
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-06 20:05:57 -0400

    feat: write initial rspec describe block

---
+++ spec/ship_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+RSpec.describe Ship do
+
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect()
+    end
+  end
+end
