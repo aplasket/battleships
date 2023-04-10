44c6b6a86f71e414432ec35709ed952a5a301baf
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-10 14:32:53 -0600

    added game to spec helper

---
+++ battleship_runner.rb
@@ -0,0 +1,6 @@
+require './spec/spec_helper'
+
+game = Game.new
+
+game.start
+
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,26 @@
 require './spec/spec_helper'
 
 class Game
+  def start
+    main_menu
+    play_game
+  end
 
+  def main_menu
+    # input = ""
+    p "Welcome to BATTLESHIP"
+    p "Enter p to play. Enter q to quit."
+    input = get.chomp.downcase
+    if input == "p"
+      play_game
+    else
+      "Press 'p' when ready to play"
+    end
+    
+  end
+
+  def play_game
+
+  end
 
 end
