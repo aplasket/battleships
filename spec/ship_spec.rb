6bb49a2462cc3f98688abdc69925d4005bf2c0b5
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-12 16:32:13 -0400

    delete additional space and change double quotes to single

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe Ship do
-
   before(:each) do
-    @cruiser = Ship.new("Cruiser", 3)
+    @cruiser = Ship.new('Cruiser', 3)
   end
 
   describe '#initialize' do
@@ -12,7 +11,7 @@ RSpec.describe Ship do
     end
 
     it 'has attributes' do
-      expect(@cruiser.name).to eq("Cruiser")
+      expect(@cruiser.name).to eq('Cruiser')
       expect(@cruiser.length).to eq(3)
     end
   end
