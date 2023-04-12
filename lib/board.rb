84761530fbb6c1e1765152c30cfcf121683e0525
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-12 16:46:19 -0400

    change double quotes to single quotes where possible

--- lib/board.rb
+++ lib/board.rb
@@ -2,22 +2,22 @@ class Board
   attr_reader :cells
   def initialize
     @cells = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4")
+      'A1' => Cell.new('A1'),
+      'A2' => Cell.new('A2'),
+      'A3' => Cell.new('A3'),
+      'A4' => Cell.new('A4'),
+      'B1' => Cell.new('B1'),
+      'B2' => Cell.new('B2'),
+      'B3' => Cell.new('B3'),
+      'B4' => Cell.new('B4'),
+      'C1' => Cell.new('C1'),
+      'C2' => Cell.new('C2'),
+      'C3' => Cell.new('C3'),
+      'C4' => Cell.new('C4'),
+      'D1' => Cell.new('D1'),
+      'D2' => Cell.new('D2'),
+      'D3' => Cell.new('D3'),
+      'D4' => Cell.new('D4')
     } 
   end
 
@@ -38,17 +38,21 @@ class Board
     coordinates.each {|coordinate| letters << coordinate.split("").first.ord}
     coordinates.each {|coordinate| numbers << coordinate.split("").last.to_i}
     if ship.length == 3
-      return false unless letters.each_cons(3).any? {|a,b,c| a - b == 0 && b - c == 0} || letters.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1}
-      return false unless numbers.each_cons(3).any? {|a,b,c| a - b == 0 && b - c == 0} || numbers.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1}
+      return false unless letters.each_cons(3).any? {|a,b,c| a - b == 0 && b - c == 0} || 
+                          letters.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1}
+      return false unless numbers.each_cons(3).any? {|a,b,c| a - b == 0 && b - c == 0} || 
+                          numbers.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1}
       return false unless same_coordinates_check(coordinates)
-      return true unless numbers.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1} && letters.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1}
+      return true unless  numbers.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1} && 
+                          letters.each_cons(3).any? {|a,b,c| b == a + 1 && c == b + 1}
     elsif ship.length == 2
-      return false unless letters.each_cons(2).any? {|a,b| a - b == 0} || letters.each_cons(2).any? {|a,b| b == a + 1}
-      return false unless numbers.each_cons(2).any? {|a,b| a - b == 0} || numbers.each_cons(2).any? {|a,b| b == a + 1}
+      return false unless letters.each_cons(2).any? {|a,b| a - b == 0} || 
+                          letters.each_cons(2).any? {|a,b| b == a + 1}
+      return false unless numbers.each_cons(2).any? {|a,b| a - b == 0} || 
+                          numbers.each_cons(2).any? {|a,b| b == a + 1}
       return false unless same_coordinates_check(coordinates)
-      return true unless numbers.each_cons(2).any? {|a,b| b == a + 1} && letters.each_cons(2).any? {|a,b| b == a + 1}
-    else
-      false
+      return true unless  numbers.each_cons(2).any? {|a,b| b == a + 1} && 
+                          letters.each_cons(2).any? {|a,b| b == a + 1}
     end
   end
     
@@ -71,28 +75,21 @@ class Board
   def render (argument = false)
     if argument == true 
       "  1 2 3 4 \n" +
-      "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" +
-      "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" +
-      "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
-      "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
+      "A #{@cells['A1'].render(true)} #{@cells['A2'].render(true)} #{@cells['A3'].render(true)} #{@cells['A4'].render(true)} \n" +
+      "B #{@cells['B1'].render(true)} #{@cells['B2'].render(true)} #{@cells['B3'].render(true)} #{@cells['B4'].render(true)} \n" +
+      "C #{@cells['C1'].render(true)} #{@cells['C2'].render(true)} #{@cells['C3'].render(true)} #{@cells['C4'].render(true)} \n" +
+      "D #{@cells['D1'].render(true)} #{@cells['D2'].render(true)} #{@cells['D3'].render(true)} #{@cells['D4'].render(true)} \n"
     else
       "  1 2 3 4 \n" +
-      "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" +
-      "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" +
-      "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" +
-      "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
+      "A #{@cells['A1'].render} #{@cells['A2'].render} #{@cells['A3'].render} #{@cells['A4'].render} \n" +
+      "B #{@cells['B1'].render} #{@cells['B2'].render} #{@cells['B3'].render} #{@cells['B4'].render} \n" +
+      "C #{@cells['C1'].render} #{@cells['C2'].render} #{@cells['C3'].render} #{@cells['C4'].render} \n" +
+      "D #{@cells['D1'].render} #{@cells['D2'].render} #{@cells['D3'].render} #{@cells['D4'].render} \n"
     end
   end       
 
 end
 
-#edge case - can't add/place a ship that is > 3
-#possible edge case - do we need to make the place method be as such that 
-##   it can't even place a ship if it's overlapping?
-## right now it's only returning false when we call @board.valid_valid_placement?(@submarine, ["A1", "B1"])
-## but when we call @board.place(@submarine, ["A1", "B1"]) for spec tst rb:78 it returns an array of coordinates
+
+
 # do we want to consider changing to all double quotes or single quotes? We seem to be going back and forth (probably because we are copying from the Iteration)
-## edge case for game - writing statements for why coordinates are not valid (e.g. not enough coordinates)
-# Ask Dani about game_spec, what else needs to be added?
-# Ask Dani about The turn in Iteration 3, can we have it where player fires and then knows the result before computer fires?
-# Player can't choose same result
