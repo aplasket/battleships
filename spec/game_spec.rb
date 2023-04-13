3a1a517f42c9b9d05180527eba3a3eb1d5178c08
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-13 14:07:34 -0400

    Add end to assertion test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,9 +14,10 @@ RSpec.describe Game do
       expect(@game.computer).to be_a(Computer)
       expect(@game.player).to be_a(Player)
     end
-    
+
     it 'starts with 0 sunken ships' do
       expect(@game.player_sunken_ships).to eq(0)
       expect(@game.computer_sunken_ships).to eq(0)
+    end
   end
 end
